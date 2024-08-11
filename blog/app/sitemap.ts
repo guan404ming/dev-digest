1c053564defbff0d79b47c6c9462fd9b3149776d
GUAN MING <guanmingchiu@gmail.com>
2024-08-12 02:42:10 +0800

    chore: remove trailing /

--- blog/app/sitemap.ts
+++ blog/app/sitemap.ts
@@ -1,6 +1,6 @@
 import { getBlogPosts } from 'app/blog/utils'
 
-export const baseUrl = 'https://blog.guan404ming.com/'
+export const baseUrl = 'https://blog.guan404ming.com'
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
