52313475c39a2eeeb8dbc6ebeab845d2f616f95f
GUAN MING <guanmingchiu@gmail.com>
2024-08-20 16:12:13 +0800

    feat(#3): update bio

--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -1,15 +1,36 @@
 import { BlogPosts } from "app/components/posts";
 
+function Badge(props) {
+  return (
+    <a
+      {...props}
+      target="_blank"
+      className="inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100"
+    />
+  );
+}
+
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Blog
+        Hey, I'm Wesley 👋
       </h1>
-      <p className="mb-4">
-        {`I'm Wesley Chiu, an Information Management student at National Taiwan University. 
-          I have a deep passion for new technologies and love finding creative ways to solve complex challenges. 
-          I'm always excited to learn and innovate in the ever-evolving world of technology.`}
+      <p className="mb-4 leading-7">
+        {`I'm a web developer & open source enthusiast. I'm currently study information management at `}
+        <span className="not-prose">
+          <Badge href="https://www.ntu.edu.tw/">
+            <img
+              alt="Next.js logomark"
+              src="/ntu.png"
+              className="!mr-1"
+              width="12"
+              height="12"
+            />
+            NTU
+          </Badge>
+        </span>
+        {`. I have a deep passion for new technologies and love finding creative ways to solve complex challenges.`}
       </p>
       <div className="my-8">
         <BlogPosts />
