852107e71a3379260161ae14f4d174218aae7a5a
GUAN MING <guanmingchiu@gmail.com>
2024-09-15 22:15:34 +0800

    fix: update space

--- frontend/app/components/posts.tsx
+++ frontend/app/components/posts.tsx
@@ -21,8 +21,8 @@ export function BlogPosts() {
             className="flex flex-col space-y-1 mb-4"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[150px] tabular-nums">
+            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-12">
+              <p className="text-neutral-600 dark:text-neutral-400 w-[150px] tabular-nums whitespace-nowrap">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tigh underline-offset-4 underline decoration-[0.5px]">
